6ae1b202ff819916c1cc782e1636e04355b7a89e
yeseung <hayeseung@gmail.com>
2024-12-26 12:16:14 +0900

    first commit

---
+++ src/main/java/com/gongdaeoppa/email/AccountController.java
@@ -0,0 +1,21 @@
+package com.gongdaeoppa.email;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class AccountController {
+    
+    private final EmailService emailService;
+    
+    @GetMapping("login/mail_confirm")
+    @ResponseBody
+    public String mailConfirm(@RequestParam String email) throws Exception {
+        String code = emailService.sendSimpleMessage(email);
+        log.info("인증코드 : " + code);
+        return code;
+    }
+}
